559d9d829f0ce02335dab68696e16a40f04c1ea5
Douglas Santos <engdouglasps@gmail.com>
2025-08-02 10:01:07 -0300

    adding color palette

--- entities/Span.js
+++ entities/Span.js
@@ -4,9 +4,4 @@ export class Span extends Component {
   constructor(parent, options) {
     super("span", parent, options);
   }
-
-  setText(text) {
-    this.getElement().textContent = text;
-    return this;
-  }
 }
